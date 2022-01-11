3ad914e1d417bcc875dc1573e2f7d89730f973d4
1067147135 <87936424+1067147135@users.noreply.github.com>
2022-01-12 00:31:42 +0800

    Update ver2.cpp

--- online-stock-span/ver2.cpp
+++ online-stock-span/ver2.cpp
@@ -1,23 +1,18 @@
 class StockSpanner {
 public:
     stack<pair<int, int>> monotone;
+    int t = 0; // time slot
     StockSpanner() {
-        monotone.push(make_pair(INT_MAX, 0));
+        monotone.push(make_pair(INT_MAX, t++));
     }
     
     int next(int price) {
-        if (monotone.top().first > price) {
-            monotone.push(make_pair(price, 1));
+        while (monotone.top().first <= price){
+            monotone.pop();
         }
-        else {
-            int count = 1;
-            while (monotone.top().first <= price){
-                count += monotone.top().second;
-                monotone.pop();
-            }
-            monotone.push(make_pair(price, count));
-        }
-        return monotone.top().second;
+        int result = t - monotone.top().second;
+        monotone.push(make_pair(price, t++));
+        return result;
     }
 };
 
